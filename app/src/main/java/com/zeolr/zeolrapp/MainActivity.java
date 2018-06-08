ca49390a88a96c4e6d529c9818b36d49ec268cfe
Ajay kumar Saini <ajay@zeolr.com>
2018-06-09 00:34:56 +0530

    First commit

---
+++ app/src/main/java/com/zeolr/zeolrapp/MainActivity.java
@@ -0,0 +1,19 @@
+package com.zeolr.zeolrapp;
+
+import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.Toast;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
